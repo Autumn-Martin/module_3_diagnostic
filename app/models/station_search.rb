0befb25d15436b00ddbd4a3c67579221903049b0
Autumn-Martin <autumnmartin.dev@gmail.com>
2018-10-17 10:19:07 -0600

    Note url from postman

--- app/models/station_search.rb
+++ app/models/station_search.rb
@@ -4,6 +4,6 @@ class StationSearch
   end
 
   def stations
-    
+    # https://developer.nrel.gov/api/alt-fuel-stations/v1/nearest.json?location=80203&fuel_type=ELEC, LPG&limit=10&api_key=ENV["api_key"]
   end
 end
