f137fb86b0c4fcdffbff5146a4917e4113f531fc
Autumn-Martin <autumnmartin.dev@gmail.com>
2018-10-17 10:37:10 -0600

    wip faraday & conn

--- app/models/station_search.rb
+++ app/models/station_search.rb
@@ -4,6 +4,13 @@ class StationSearch
   end
 
   def stations
-    # https://developer.nrel.gov/api/alt-fuel-stations/v1/nearest.json?location=80203&fuel_type=ELEC, LPG&limit=10&api_key=ENV["api_key"]
+    # https://developer.nrel.gov/api/alt-fuel-stations/v1/nearest.json?location=80203&fuel_type=ELEC, LPG&limit=10&api_key=ENV["nrel_api_key"]
+    conn = Faraday.new(url: "https://developer.nrel.gov/v1/nearest.json?") do |faraday|
+      faraday.params["location"] = "80203"
+      faraday.params["fuel_type"] = "ELEC, LPG"
+      faraday.params["limit"] = 10
+      faraday.params["api_key"] = ENV["nrel_api_key"]
+      faraday.adapter Faraday.default_adapter
+    end
   end
 end
