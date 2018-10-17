7463531581c236271faf6e9358ba8d4ef39cb26d
Autumn-Martin <autumnmartin.dev@gmail.com>
2018-10-17 11:10:48 -0600

    complete refactor

--- app/models/station_search.rb
+++ app/models/station_search.rb
@@ -8,26 +8,9 @@ class StationSearch
     data[:fuel_stations].map do |station_data|
       Station.new(station_data)
     end
-
-
-    # https://developer.nrel.gov/api/alt-fuel-stations/v1/nearest.json?location=80203&fuel_type=ELEC, LPG&limit=10&api_key=ENV["nrel_api_key"]
-    conn = Faraday.new(url: "https://developer.nrel.gov") do |faraday|
-      faraday.params["location"] = "80203"
-      faraday.params["fuel_type"] = "ELEC, LPG"
-      faraday.params["limit"] = 10
-      faraday.params["api_key"] = ENV["nrel_api_key"]
-      faraday.adapter Faraday.default_adapter
-    end
-
-    response = conn.get("/api/alt-fuel-stations/v1/nearest.json")
-    data = JSON.parse(response.body, symbolize_names: true)
-
-    data[:fuel_stations].map do |station_data|
-      Station.new(station_data)
-    end
   end
 
   def service
     NrelService.new(@zip_code)
-  end 
+  end
 end
